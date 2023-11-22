b23dbe33488b3f93e2c87846f7661cea495f7afc
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-22 11:58:10 -0700

    Create Hobbit class and define methods to pass the tests

---
+++ ruby_exercises/mythical-creatures/lib/hobbit.rb
@@ -0,0 +1,21 @@
+class
+    Hobbit
+    attr_reader :name, :age, :disposition
+
+    def initialize (name, disposition = 'homebody')
+        @name = name
+        @disposition = disposition
+        @age = 0
+    end
+
+    def celebrate_birthday
+        @age += 1
+    end
+    def  adult?
+        @age > 32
+    end
+
+    def old?
+        @age >= 101
+    end
+end
