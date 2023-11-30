657c7986c0ed142d6162a26ee0ccaeab563908ae
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 12:17:26 -0700

    Tried solving hashes excersies

--- lessons/nested_collections/Examples/array_within_hash.rb
+++ lessons/nested_collections/Examples/array_within_hash.rb
@@ -5,6 +5,11 @@ pizza_toppings = {
 }
 
 # 1. what is pizza_toppings.count
+puts pizza_toppings.count
 # 2. what is pizza_toppings.values
+puts pizza_toppings.values
 # 3. how can I access the element “pineapple”
+puts pizza_toppings.values[-1]
 # 4. how can I add the element “olives” to the key “veggies”?
+pizza_toppings[:veggies].push("olives")
+puts pizza_toppings.values[0]
--- lessons/nested_collections/Examples/hash_within_hash.rb
+++ lessons/nested_collections/Examples/hash_within_hash.rb
@@ -2,8 +2,12 @@ pets = {:dog => {:name => "Chance", :weight => "45 pounds"},
         :cat => {:name => "Sassy", :weight => "15 pounds"}
        }
 
-# 1. what is pets.count
-# 2. what is pets.keys
-# 3. what is pets.values
+# 1. what is pets.count. 2?
+puts pets.count
+# 2. what is pets.keys god and cat
+puts pets.keys
+# 3. what is pets.values Chance, 45pounds, Sassy, 15pounds
+puts pets.values
 # 4. how can I access the element "15 pounds"
+
 # 5. how can I add :age => 3 to the value of the key :dog
