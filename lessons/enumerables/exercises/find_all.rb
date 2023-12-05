cb75c2ed24e604cb16492ca4aa197e8968024569
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-05 11:52:58 -0700

    feat: Executed find_all excercises

--- lessons/enumerables/exercises/find_all.rb
+++ lessons/enumerables/exercises/find_all.rb
@@ -6,9 +6,27 @@ def find_all_nums_divisible_by_3
     numbers = [1,2,3,4,5,6,7,8,9]
     
     ### YOUR CODE HERE
-  
+    nums_div_by_three = []
+
+    numbers.each do |num|
+        if (num % 3 )== 0
+            nums_div_by_three << num
+        end
+    end
+    nums_div_by_three
     # should return [3,6,9]
 end
 
+p find_all_nums_divisible_by_3
+
+def find_all_nums_divisible_by_3
+    numbers = [1,2,3,4,5,6,7,8,9]
+    
+    ### YOUR CODE HERE
+    numbers.find_all do |num|
+        num % 3 == 0     
+    end
+    # should return [3,6,9]
+end
 
 p find_all_nums_divisible_by_3
