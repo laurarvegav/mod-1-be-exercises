58e65e6069a8b043e18e350dff1a95483d64ad1f
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-06 11:32:58 -0700

    feat: Create get_all_from_category method for Potluck class

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -10,4 +10,11 @@ class Potluck
     def add_dish(dish)
         @dishes << dish
     end
+
+    def get_all_from_category(cat)
+        
+        @dishes.find_all do |dish|
+            dish.category == cat
+        end
+    end
 end
