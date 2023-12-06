32f1b26fd319e7b2db7c6da9fa4ec565d6892f05
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-06 11:32:28 -0700

    test: Create the can return all dishes from one category test

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe Potluck do
         expect(potluck.date).to eq ("7-13-18")
     end
 
+    it "starts with no dishes" do
+        potluck = Potluck.new("7-13-18")
+        expect(potluck.dishes).to eq ([])
+    end
+
     it "can add dishes and store them" do
         potluck = Potluck.new("7-13-18")
         couscous_salad = Dish.new("Couscous Salad", :appetizer)
@@ -26,8 +31,23 @@ RSpec.describe Potluck do
         expect(potluck.dishes.length).to eq(2)
     end
 
-    it "starts with no dishes" do
+    it "can return all dishes from one category" do
         potluck = Potluck.new("7-13-18")
-        expect(potluck.dishes).to eq ([])
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        roast_pork = Dish.new("Roast Pork", :entre)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        candy_salad = Dish.new("Candy Salad", :dessert)
+        potluck.add_dish(couscous_salad)
+        potluck.add_dish(summer_pizza)
+        potluck.add_dish(roast_pork)
+        potluck.add_dish(cocktail_meatballs)
+        potluck.add_dish(candy_salad)
+
+        expect(potluck.get_all_from_category(:appetizer)).to eq([couscous_salad,summer_pizza])
+    end  
+
+    it "returns a menu" do
+
     end
 end
