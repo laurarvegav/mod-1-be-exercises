9abe1e9216114ee645610e8b5e57d062c9469265
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-06 14:17:21 -0700

    feat: Create get_names_from_category helper method for Potluck class, also menu and ratio methods

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -5,6 +5,7 @@ class Potluck
     def initialize (date)
         @date = date
         @dishes =[]
+        @menu = {}
     end
 
     def add_dish(dish)
@@ -12,9 +13,29 @@ class Potluck
     end
 
     def get_all_from_category(cat)
-        
         @dishes.find_all do |dish|
             dish.category == cat
         end
+    end
+
+    def get_names_from_category(cat)
+        @dishes_from_category = []
+        @dishes.each do |dish|
+            if dish.category == cat
+                @dishes_from_category << dish.name
+            end
+        end
+        @dishes_from_category
+    end
+
+    def menu 
+        @dishes.each do |dish|
+            @menu[dish.category] = get_names_from_category(dish.category).sort
+        end
+        @menu
+    end
+
+    def ratio(cat)
+        ((get_names_from_category(cat).count).to_f/(@dishes.count))*100
     end
 end
