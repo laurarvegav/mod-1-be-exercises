980ca8a1edc190118f0a332cf2b45bce6cce2f0a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-06 11:13:59 -0700

    test: Create the can add dishes and store them tess

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -1,5 +1,6 @@
 require "rspec"
 require "./lib/potluck"
+require "./lib/dish"
 
 RSpec.describe Potluck do
 
@@ -11,6 +12,18 @@ RSpec.describe Potluck do
     it "stores a date" do
         potluck = Potluck.new("7-13-18")
         expect(potluck.date).to eq ("7-13-18")
+    end
+
+    it "can add dishes and store them" do
+        potluck = Potluck.new("7-13-18")
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        potluck.add_dish(couscous_salad)
+        potluck.add_dish(cocktail_meatballs)
+
+        expect(potluck.dishes).to eq([couscous_salad,cocktail_meatballs])
+
+        expect(potluck.dishes.length).to eq(2)
     end
 
     it "starts with no dishes" do
