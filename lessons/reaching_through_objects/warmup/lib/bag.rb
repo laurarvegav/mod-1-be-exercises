1cd1b9aee8243eee12305e5fa27d230f5c3c2d61
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-11 10:33:05 -0700

    feat: Create Bag class, its attributes, needed methods to make tests pass

---
+++ lessons/reaching_through_objects/warmup/lib/bag.rb
@@ -0,0 +1,37 @@
+class Bag
+    attr_reader :candies
+
+    def initialize
+        @candies = []
+    end
+
+    def empty?
+        @candies.empty?
+    end
+
+    def count
+        @candies.count
+    end
+
+    def add_candy(candy)
+        @candies << candy
+    end
+
+
+    def contains?(candy_type)
+        # OPTION1
+        # @candies.any? do |candy|
+        #     candy.type == candy_type
+        # end
+
+        # OPTION2
+        # evaluation = false
+        # @candies.each do |candy|
+        #     if candy.type == candy_type
+        #         evaluation = true
+        #     end
+        # end
+        # evaluation
+    end
+
+end
