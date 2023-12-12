0b5ef33b6b05763e631dd283ede4e07cb078698c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-12 10:27:43 -0700

    Solve

--- lessons/nested_collections/Examples/hash_within_hash.rb
+++ lessons/nested_collections/Examples/hash_within_hash.rb
@@ -9,5 +9,7 @@ puts pets.keys
 # 3. what is pets.values Chance, 45pounds, Sassy, 15pounds
 puts pets.values
 # 4. how can I access the element "15 pounds"
-
+puts pets[:cat][:weight]
 # 5. how can I add :age => 3 to the value of the key :dog
+pets[:dog][:age] = 3
+puts pets
--- lessons/nested_collections/Examples/hashes_within_arrays.rb
+++ lessons/nested_collections/Examples/hashes_within_arrays.rb
@@ -1,6 +1,8 @@
 food_feelings = [{:pizza => "tasty"}, {:calzone => "also tasty"}]
 
-# 1. what is food_feelings.count
-# 2. what is food_feelings.first.count
-# 3. how can I access the value "also tasty"
+# 1. what is food_feelings.count -> 2
+# 2. what is food_feelings.first.count -> 1
+# 3. how can I access the value "also tasty" 
+food_feelings.last[:calzone]
 # 4. how can I change the value “also tasty” to “super delicious”
+food_feelings.last[:calzone] = "super delicious"
