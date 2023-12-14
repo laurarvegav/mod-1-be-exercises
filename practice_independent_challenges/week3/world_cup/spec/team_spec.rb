2f06252fd109d5d0a6ee494c577f43fa31d39631
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-14 14:19:07 -0700

    Create tests for Team class

--- practice_independent_challenges/week3/world_cup/spec/team_spec.rb
+++ practice_independent_challenges/week3/world_cup/spec/team_spec.rb
@@ -3,10 +3,45 @@ require "./lib/player"
 require"./lib/team"
 
 RSpec.describe Team do
-    team = Team.new("France")
+    it "exists" do
+        team = Team.new("France")
 
-    expect(team.country).to eq("France")
-    expect(team.eliminated?).to be(false)
-    expect(team.players).to eq([])
+        expect(team).to be_a(Team)
+        expect(team.country).to eq("France")
+        expect(team.players).to eq([])
+    end
     
+    it "can return correct #eliminated? status" do
+        team = Team.new("France")
+    
+        expect(team.eliminated?).to be(false)
+        team.eliminated = true
+
+        expect(team.eliminated?).to be(true)
+    end
+
+    it "can get players added to a team" do
+        team = Team.new("France")
+        mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+        pogba = Player.new({name: "Paul Pogba", position: "midfielder"})    
+        team.add_player(mbappe)
+        team.add_player(pogba)    
+
+        expect(team.players).to eq([mbappe , pogba])
+    end
+
+    it "can return the players in certain position" do
+        team = Team.new("France")
+        mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+        pogba = Player.new({name: "Paul Pogba", position: "midfielder"})    
+        team.add_player(mbappe)
+        team.add_player(pogba)
+        
+        expect(team.players_by_position("midfielder")).to eq([pogba])
+        expect(team.players_by_position("defender")).to eq ([])
+    end
+
+
+
+
 end
