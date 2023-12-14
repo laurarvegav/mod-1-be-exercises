c7550ae1714223003d46dc0922a0022f9ee724e0
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-14 16:07:26 -0700

    Fix: expected line for last test

--- practice_independent_challenges/week3/world_cup/spec/world_cup_spec.rb
+++ practice_independent_challenges/week3/world_cup/spec/world_cup_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe WorldCup do
         croatia.add_player(modric)
         croatia.add_player(vida)
         world_cup = WorldCup.new(2018, [france, croatia])
-        expect(world_cup.all_players_by_position).to eq({"forward":[mbappe], "midfielder":[modric], "defender":[vida]})
+        expect(world_cup.all_players_by_position).to eq({"forward":[mbappe], "midfielder":[pogba,modric], "defender":[vida]})
     end
 
 end
