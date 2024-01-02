85e93cde9c59269ec050e43ce27cfd93e4bfe66c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-02 06:47:27 -0700

    Create methods and Contestant class

---
+++ practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
@@ -0,0 +1,24 @@
+class Contestant
+    attr_reader :full_name, 
+                :age,
+                :state_of_residence,
+                :spending_money,
+                :game_interests
+    
+    def initialize(contestant)
+        @full_name = "#{contestant[:first_name]} #{contestant[:last_name]}"
+        @age = contestant[:age]
+        @state_of_residence = contestant[:state_of_residence]
+        @spending_money = contestant[:spending_money]
+        @game_interests = []
+    end
+
+    def out_of_state?
+        @state_of_residence != "CO"
+    end
+
+    def add_game_interest(game)
+        @game_interests << game
+    end
+
+end
