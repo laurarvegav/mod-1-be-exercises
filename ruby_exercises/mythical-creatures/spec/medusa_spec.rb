93e3c2e2ad527f27851a897f49cb656afb48f791
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-01 19:02:05 -0700

    Tests for Medusa class

--- ruby_exercises/mythical-creatures/spec/medusa_spec.rb
+++ ruby_exercises/mythical-creatures/spec/medusa_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require './lib/medusa'
+require './lib/person'
 
 RSpec.describe Medusa do
   it 'has a name' do
@@ -32,10 +33,33 @@ RSpec.describe Medusa do
   end
 
   it 'can only have three victims' do
-    # your code here
+    medusa = Medusa.new('Cassiopeia')
+    victim1 = Person.new('Perseus')
+    victim2 = Person.new('Hercules')
+    victim3 = Person.new('Aquiles')
+    victim4 = Person.new('Xie')
+
+    medusa.stare(victim1)
+    medusa.stare(victim2)
+
+    expect(medusa.statues).to eq([victim1,victim2])
+    medusa.stare(victim3)
+    expect(medusa.statues).to eq([victim1,victim2,victim3])
   end
 
   it 'if a fourth victim is stoned the first is unstoned' do
-    # your code here
+    medusa = Medusa.new('Cassiopeia')
+    victim1 = Person.new('Perseus')
+    victim2 = Person.new('Hercules')
+    victim3 = Person.new('Aquiles')
+    victim4 = Person.new('Xie')
+
+    medusa.stare(victim1)
+    medusa.stare(victim2)
+    medusa.stare(victim3)
+    expect(medusa.statues).to eq([victim1,victim2,victim3])
+    medusa.stare(victim4)
+    expect(medusa.statues).to eq([victim2,victim3,victim4])
+    expect(victim1.stoned?).to be false
   end
 end
