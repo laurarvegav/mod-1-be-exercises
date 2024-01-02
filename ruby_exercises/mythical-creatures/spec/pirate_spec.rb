c14874647f12e06efc4b493ef7070eeee7ff972e
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-01 18:40:02 -0700

    Tests for Pirate class

--- ruby_exercises/mythical-creatures/spec/pirate_spec.rb
+++ ruby_exercises/mythical-creatures/spec/pirate_spec.rb
@@ -39,13 +39,20 @@ RSpec.describe Pirate do
 
   it 'has a booty' do
     # create a pirate
+    pirate = Pirate.new('Jack')
     # check that the pirate starts with 0 booty
+    expect(pirate.booty).to eq(0)
   end
 
   it 'gets 100 booty for robbing a ship' do
     # create a pirate
+    pirate = Pirate.new('Jack')
     # rob some ships
+    pirate.rob
+    pirate.rob
+    pirate.rob
     # check that the pirate got 100 booty for each ship it robbed
+    expect(pirate.booty).to eq(300)
   end
 
 end
