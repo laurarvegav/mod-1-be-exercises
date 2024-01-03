20e7170a4bf998ea4136d573ec6f779f06a4f76b
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-03 10:17:37 -0700

    play with file

--- lessons/class_methods/exercise_1/1runner.rb
+++ lessons/class_methods/exercise_1/1runner.rb
@@ -4,14 +4,14 @@ max = Dog.new("Max")
 puts max.woof
 
 ## Describe what each part of the code above did -
-# `max` =>
-# `Dog` =>
-# `.woof` =>
+# `max` => Initialize Dog instance with name Max
+# `Dog` => Called the class Dog
+# `.woof` => called the method woof on max instance
 
-# puts Dog.woof
+puts Dog.woof
 
 ## Is there any difference between the two methods above? (max.woof and Dog.woof)?
 
 ## Un-comment the line of code below, does it work? Why or why not?
-# Dog.bark
+puts max.bark
 
