1d7bcf0bdd3183205bed7dcae647cf134c5ce1ab
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-13 21:17:09 -0700

    Feat: Add artists_and_photographs, artists_with_more_than_two_photographs, photograps_from_country(country) methods for Cutrator class

--- practice_independent_challenges/week5/museo/lib/curator.rb
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -1,23 +1,63 @@
 class Curator
-    attr_reader :photographs,
-                :artists
+  attr_reader :photographs,
+              :artists
+  
+  def initialize
+    @photographs = []
+    @artists = []
+  end
+
+  def add_photograph(photograph)
+    @photographs << photograph
+  end
+
+  def add_artist(artist)
+    @artists << artist
+  end
+
+  def find_artist_by_id(id)
+    @artists.find do |artist|
+      artist.id == id
+    end
+  end
+
+  def artists_and_photographs
+    hash = Hash.new([])
     
-    def initialize
-        @photographs = []
-        @artists = []
+    @artists.each do |artist|
+      hash[artist] = []
     end
-
-    def add_photograph(photograph)
-        @photographs << photograph
-    end
-
-    def add_artist(artist)
-        @artists << artist
-    end
-
-    def find_artist_by_id(id)
-        @artists.find do |artist|
-            artist.id == id
+ 
+    @photographs.each do |photograph|
+      @artists.each do |artist|
+        if photograph.artist_id == artist.id
+          hash[artist] << photograph 
         end
+      end
+      hash
     end
+    hash
+  end
+
+  def artists_with_more_than_two_photographs
+    name = "a"
+    @artists.each do |artist|
+      if artists_and_photographs[artist].size >= 2
+        name = artist.name
+      end
+    end
+    name
+  end
+
+  def photograps_from_country(country)
+    photo = []
+    @artists.each do |artist|
+      if artist.country == country
+        @photographs.each do |photograph|
+          photo << photograph.name if photograph.artist_id == artist.id
+        end
+      end
+    end
+    photo.join(",")
+  end
 end
